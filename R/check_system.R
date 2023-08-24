c30b7c265aa617149eeca2d09c24ffc4c532a35d
Veronica M White <30372917+vmwhite@users.noreply.github.com>
2023-08-24 09:54:28 -0500

    Update check_system.R

--- R/check_system.R
+++ R/check_system.R
@@ -11,9 +11,9 @@
 #' check_system(rho_q, rho_p)
 check_system<- function(rho_q, rho_p){
   if (rho_q > 1){
-    paste0("ERROR - utilization of secondary servers is ", rho_q_check, " must be below 1. please adjust N, P_1, or lambda lower and/or mu_2 higher.")
+    paste0("ERROR - utilization of secondary servers is ", rho_q, " must be below 1. please adjust N, P_1, or lambda lower and/or mu_2 higher.")
   }else if (rho_p > 1){
-    paste0("ERROR - utilization of primary servers is ", rho_p_check, " must be below 1. please adjust lambda lower and/or N or mu_2 higher.")
+    paste0("ERROR - utilization of primary servers is ", rho_p, " must be below 1. please adjust lambda lower and/or N or mu_2 higher.")
   }else{
     paste0("Calculating Metrics - model inputs are valid.")
   }
